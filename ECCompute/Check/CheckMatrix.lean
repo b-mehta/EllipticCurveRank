8727528baa457d5d6416dd0ef3217c44ba59f9c2
Bhavik Mehta <bhavikmehta8@gmail.com>
2026-08-20 14:56:12 +0000

    License the project under GPL version 3.0

--- ECCompute/Check/CheckMatrix.lean
+++ ECCompute/Check/CheckMatrix.lean
@@ -1,6 +1,6 @@
 /-
 Copyright (c) 2026 Bhavik Mehta. All rights reserved.
-Released under Apache 2.0 license as described in the file LICENSE.
+Released under the GNU General Public License version 3.0 as described in the file LICENSE.
 Authors: Bhavik Mehta
 -/
 import ECCompute.Check.F2Invert
