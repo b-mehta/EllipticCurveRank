52f0b3dfae22b87cd505d331e0c7e25981394448
Bhavik Mehta <bhavikmehta8@gmail.com>
2026-08-22 19:41:14 +0000

    Drop the redundant Kernel import (arrives via Fold)

--- ECCompute/Soundness/F2Invert.lean
+++ ECCompute/Soundness/F2Invert.lean
@@ -3,7 +3,6 @@ Copyright (c) 2026 Bhavik Mehta. All rights reserved.
 Released under the GNU General Public License version 3.0 as described in the file LICENSE.
 Authors: Bhavik Mehta
 -/
-import ECCompute.Kernel
 import ECCompute.Soundness.Fold
 import Mathlib.Data.Nat.Bitwise
 import Mathlib.Data.ZMod.Basic
