f21c00b5ef929422b63467ecde108db153e67382
Bhavik Mehta <bhavikmehta8@gmail.com>
2026-08-22 19:41:17 +0000

    Drop the redundant Kernel import (arrives via Fold)

--- ECCompute/Soundness/Primes.lean
+++ ECCompute/Soundness/Primes.lean
@@ -3,7 +3,6 @@ Copyright (c) 2026 Bhavik Mehta. All rights reserved.
 Released under the GNU General Public License version 3.0 as described in the file LICENSE.
 Authors: Bhavik Mehta
 -/
-import ECCompute.Kernel
 import ECCompute.Soundness.Fold
 import ECCompute.ForLean
 import Mathlib.Data.Nat.Prime.Defs
