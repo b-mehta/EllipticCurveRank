b4a271abb0b23a67c669a42322c034d505a4e0b0
Bhavik Mehta <bhavikmehta8@gmail.com>
2026-08-28 17:52:24 +0100

    Move the explicit curves into a separate Curves library (#385)
    * Move the explicit curves into a separate Curves library
    
    * Update the README for the separate Curves library

---
+++ Curves.lean
@@ -0,0 +1,13 @@
+module  -- shake: keep-all --deprecated_module: ignore
+
+public import Curves.Curve10
+public import Curves.Curve11
+public import Curves.Curve12
+public import Curves.Curve13
+public import Curves.Curve14
+public import Curves.Curve273
+public import Curves.Curve302
+public import Curves.Curve7
+public import Curves.Curve74
+public import Curves.Curve8
+public import Curves.Curve9
